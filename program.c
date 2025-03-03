5b372af372d9710ba82409562d83744d7a439879
Reid Harrington <reidmh110@gmail.com>
2025-03-03 13:12:22 -0500

    update algo

--- program.c
+++ program.c
@@ -18,24 +18,42 @@ int main(int argc, char *argv[])
 
     printf("Hello from process %d out of %d\n", world_rank, world_size);
 
-    int n = atoi(argv[1]); //converts the first argument to a int (n)
+
+    int A[] = {2,4,6,8};
+    int B[] = {1,3,5,9};
+
+    int n = 4; //atoi(argv[1]); //size of each input array
     int r = n / (2 * world_size); //number of groups
-    int k = log(n); //number of elements in each group
-    int array[] = {1,2,3,4,5,6};
+    int k = (int)log(n) / log(2); //number of elements in each group
 
-    printf("%d, %d, %d\n", n,r,k);
-    for(int i = 1; i < r + 1; i++)
+
+    int target = 3;
+
+    int *split_points = NULL;
+    //rank 0 processes the split points for B
+    if(world_rank == 0)
     {
-        // int insersionPoint = binary_search()
-        //getting length array
-        int size = sizeof(array) / sizeof(array[0]);
-        int target = 3;
+        //creating the split points array, of size processors + 1
+        split_points = (int *)malloc((world_size + 1) * sizeof(int));
+        //first split point at 0
+        split_points[0] = 0; 
 
-        int index = binary_search(array, size, target);
-        printf("%d", index);
+        //find all split points based on A partition
+        for (int i = 0; i < world_size; i++)
+        {
+            int a_lastIndex = (i + 1) * (n / world_size) - 1; //last index of the current segment in A for process i
+            if (a_lastIndex >= n) a_lastIndex = n - 1; //if the last index is greater than the size of array, assign it to n-1
+            int a_end = A[a_lastIndex];
+
+
+            //finds the first index in B where the element is >= a_end. 
+            //this becomes the split point for B for process i + 1
+            split_points[i + 1] = binary_search(B, n, a_end);
+        }
     }
-
-
+    for (int i = 0; i < world_size + 1; i++) {
+        printf("%d ", split_points[i]);
+    }
 
     MPI_Finalize();
     return 0;
@@ -61,3 +79,17 @@ int binary_search(int array[], int size, int target)
     //target not found
     return -1; 
 } 
+
+/*/ ALGORITHM
+    init: MPI is initialized
+
+    global arrays: A and B are global sorted arrays.
+
+    split points: rank 0 computes split points in B using binary search based on partitions of A. These points ensure each processor merges non-overlapping sections.
+
+    broadcast: the split points are broadcasted to all processes.
+
+    local merge: each process extracts its portion of A and B, merges them sequentially, and stores the result in C_part.
+
+    results: rank 0 process gathers all C_part arrays using MPI_Gatherv to form the final merged array C.
+/*/
